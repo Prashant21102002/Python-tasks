804c06e880431d2ef99ab0a338ab418100425a5f
Prashant Mishra <153819551+Prashant21102002@users.noreply.github.com>
2025-08-10 06:40:45 +0530

    Add files via upload

---
+++ Ass2Task1.py
@@ -0,0 +1,6 @@
+a = input("Enter a number: ")
+a = int(a)
+if a % 2 == 0:
+    print(f"{a} is an even number.")
+else:
+    print(f"{a} is an odd number.")
---
+++ Ass2Task2.py
@@ -0,0 +1,2 @@
+n = list(range(1, 51))
+print(f"The sum of numbers from 1 to 50 is: {sum(n)}")
