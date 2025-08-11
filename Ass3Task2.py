ce3ef70c50185d657cd2d966683f6334305359d7
Prashant Mishra <153819551+Prashant21102002@users.noreply.github.com>
2025-08-11 13:55:28 +0530

    Add files via upload

---
+++ Ass3Task2.py
@@ -0,0 +1,11 @@
+from math import *
+
+n = float(input("Enter a number: "))
+
+sqrt_result = sqrt(n)
+log_result = log(n)
+sin_result = sin(n)
+
+print(f"Square root of: {sqrt_result}")
+print(f"Logarithm: {log_result}")
+print(f"Sine: {sin_result}")
